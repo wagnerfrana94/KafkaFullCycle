43437cb194062591da4d149549d56ca7c3f516a6
Wagner Frana <prog29@ixcsoft.com.br>
2023-09-19 22:12:44 -0300

    Inclusao de consumidor de mensagens em go.

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -6,14 +6,27 @@ import (
 )
 
 func main() {
+    deliveryChan := make(chan kafka.Event)
     producer := NewKafkaProducer()
-    Publish("mensagem", "teste", producer, nil)
+    Publish("Transferencia", "teste", producer, []byte("transferencia"), deliveryChan)
+
+    e := <-deliveryChan
+    msg := e.(*kafka.Message)
+
+    if msg.TopicPartition.Error != nil {
+        log.Println("Erro ao enviar!")
+    } else {
+        log.Println("Mensagem enviada ", msg.TopicPartition)
+    }
+
     producer.Flush(1000)
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
 		"bootstrap.servers":   "kafka:9092",
+		"delivery.timeout.ms": "0",
+		"acks": "all",
 	}
 	p, err := kafka.NewProducer(configMap)
 	if err != nil {
@@ -22,13 +35,13 @@ func NewKafkaProducer() *kafka.Producer {
 	return p
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChan chan kafka.Event) error {
 	message := &kafka.Message{
 		Value:          []byte(msg),
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Key:            key,
 	}
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChan)
 	if err != nil {
 		return err
 	}
